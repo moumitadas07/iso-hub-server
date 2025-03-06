3d7fa898b1ba533be9c032eaf06c25de6535ccf5
moumitadas07 <moumitadas.avology@gmail.com>
2025-03-06 14:37:06 +0530

    Second commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -70,4 +70,3 @@ class AuthController extends Controller
   
  }
 
-
--- database/migrations/2025_03_06_062515_add_role_to_users_table.php
+++ database/migrations/2025_03_06_062515_add_role_to_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->string('role')->default('user');
         });
     }
-
     /**
      * Reverse the migrations.
      */
